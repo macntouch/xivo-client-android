625bd16508e410dd81d28515d424c922c2c5021a
Clément Quaquin <cquaquin@proformatique.com>
2010-11-18 10:54:04 +0000

    Chargement dynamique des xlets dans des onglets
    git-svn-id: svn+ssh://rcs.lan-puteaux.avencall.com/var/lib/svn/xivo/branches/people/cquaquin/XivoClientAndroid@9587 dc85baef-7018-0410-bfbd-8a639da648bb

--- src/com/proformatique/android/xivoclient/XletsContainerTabActivity.java
+++ src/com/proformatique/android/xivoclient/XletsContainerTabActivity.java
@@ -1,13 +1,30 @@
 package com.proformatique.android.xivoclient;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.pm.ServiceInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
+import android.widget.Toast;
+
 import com.proformatique.android.xivoclient.xlets.XletIdentity;
 
 public class XletsContainerTabActivity extends TabActivity {
+
+	public static final String ACTION_XLET_LOAD = "xivo.intent.action.LOAD_XLET";
+	private static final String LOG_TAG = "XLETS_LOADING";
 	
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -18,22 +35,44 @@ public class XletsContainerTabActivity extends TabActivity {
 	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
 	    Intent intent;  // Reusable Intent for each tab
 
-	    intent = new Intent().setClass(this, XletIdentity.class);
-
-	    // Initialize a TabSpec for each tab and add it to the TabHost
-	    spec = tabHost.newTabSpec("identity").setIndicator("Identity").setContent(intent);
-	    tabHost.addTab(spec);
-
-	    spec = tabHost.newTabSpec("xlet2").setIndicator("Xlet 2").setContent(intent);
-        tabHost.addTab(spec);
-
-	    spec = tabHost.newTabSpec("xlet3").setIndicator("Xlet 3").setContent(intent);
-        tabHost.addTab(spec);
-
-        tabHost.setCurrentTab(0);
+	    tabHost.setCurrentTab(0);
         
-//        tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = ;
+        /**
+         * The PackageManager will help us to retrieve all the activities
+         * that declared an intent of type ACTION_XLET_LOAD in the Manifest file
+         */
+	    PackageManager pm = getPackageManager();
+        Intent xletIntent = new Intent( ACTION_XLET_LOAD );
+        
+        List<ResolveInfo> list = pm.queryIntentActivities(xletIntent, PackageManager.GET_RESOLVED_FILTER);
+        
+	    /**
+	     *  Initialize a TabSpec for each tab and add it to the TabHost
+	     *  Dynamic loading of each xlet into one tab
+	     */
+        for( int i = 0 ; i < list.size() ; ++i ) {
+        	
+	        ResolveInfo rInfo = list.get( i );
+			ActivityInfo aInfo = rInfo.activityInfo;
+			String label;
+			String desc;
+			
+			try {
+				label = getString(aInfo.labelRes);
+				desc = getString(aInfo.descriptionRes);
+			
+			    try {
+					intent = new Intent().setClass(this, Class.forName(aInfo.name));
+				    spec = tabHost.newTabSpec(label).setIndicator(desc).setContent(intent);
+				    tabHost.addTab(spec);
+	
+			    } catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
 
-
+			} catch (Exception e1) {
+				Log.d( LOG_TAG, "Missing label or description declaration for Xlet Activity : "+ aInfo.name);
+			}
+        }
 	}
 }
