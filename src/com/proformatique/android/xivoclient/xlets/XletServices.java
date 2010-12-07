8a6d88cdd45dfceb66d85b916048414e62630cb7
Clément Quaquin <cquaquin@proformatique.com>
2010-12-07 17:07:33 +0000

    xlet Services --> work in progress (no action behind checkboxs)
    git-svn-id: svn+ssh://rcs.lan-puteaux.avencall.com/var/lib/svn/xivo/branches/people/cquaquin/XivoClientAndroid@9713 dc85baef-7018-0410-bfbd-8a639da648bb

--- src/com/proformatique/android/xivoclient/xlets/XletServices.java
+++ src/com/proformatique/android/xivoclient/xlets/XletServices.java
@@ -1,8 +1,19 @@
 package com.proformatique.android.xivoclient.xlets;
 
+import com.proformatique.android.xivoclient.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class XletServices extends Activity implements XletInterface{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.xlet_services);
+
+	}
 	
 	
 
