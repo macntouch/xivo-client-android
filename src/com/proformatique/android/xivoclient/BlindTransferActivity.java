dac7010a03cf46fabf8e5e1bcd90ab30a862ac4a
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-01-10 16:13:35 -0500

    Registered some button clicks on blind transfer

--- src/com/proformatique/android/xivoclient/BlindTransferActivity.java
+++ src/com/proformatique/android/xivoclient/BlindTransferActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class BlindTransferActivity extends Activity implements OnClickListener {
 	
@@ -15,10 +18,18 @@ public class BlindTransferActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.blind_transfer);
 		Log.d(LOG_TAG, "onCreate");
+		
+		// GUI stuff
+		Button cancelBtn = (Button) findViewById(R.id.button_cancel_blind_tx);
+		Button transferBtn = (Button) findViewById(R.id.button_blind_tx);
+		
+		cancelBtn.setOnClickListener(this);
+		transferBtn.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
+		Log.d(LOG_TAG, "onClick");
 		switch(v.getId()) {
 		case R.id.button_cancel_blind_tx:
 			cancelClicked();
@@ -30,10 +41,16 @@ public class BlindTransferActivity extends Activity implements OnClickListener {
 	}
 	
 	private void transferClicked() {
-		Log.d(LOG_TAG, "transfer clicked");
+		EditText et = (EditText) findViewById(R.id.transfer_destination);
+		String number = et.getText().toString();
+		if (number != null && number.equals("") != true) {
+			// transfer the call here
+			Toast.makeText(getApplicationContext(), "transfering to " + number, Toast.LENGTH_LONG).show();
+			finish();
+		}
 	}
 	
 	private void cancelClicked() {
-		Log.d(LOG_TAG, "cancel clicked");
+		this.finish();
 	}
 }
