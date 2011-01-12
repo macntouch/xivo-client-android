84b433ad2ddc76dc171cef814a4be658ed179552
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-01-12 12:29:07 -0500

    Added BlindTransferJSONTask

--- src/com/proformatique/android/xivoclient/BlindTransferActivity.java
+++ src/com/proformatique/android/xivoclient/BlindTransferActivity.java
@@ -1,5 +1,9 @@
 package com.proformatique.android.xivoclient;
 
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -7,13 +11,36 @@ public class BlindTransferActivity extends TransferActivity {
 	
 	@Override
 	protected void transferClicked() {
+		Log.d(LOG_TAG, "Blind transfer clicked");
 		EditText et = (EditText) findViewById(R.id.transfer_destination);
 		String number = et.getText().toString();
 		if (number != null && number.equals("") != true) {
-			// transfer the call here
-			Toast.makeText(getApplicationContext(), getString(R.string.transfering, number), Toast.LENGTH_LONG).show();
+			new BlindTransferJSONTask().execute();
 			finish();
 		}
 	}
 	
+	/**
+	 * Creates an AsyncTask to run the call transfer
+	 */
+	private class BlindTransferJSONTask extends AsyncTask<Void, Integer, Integer> {
+		
+		private String number;
+		
+		@Override
+		protected void onPreExecute() {
+			EditText et = (EditText) findViewById(R.id.transfer_destination);
+			number = et.getText().toString().replace("-", "");
+			Log.d(LOG_TAG, "Transfering call to " + number);
+			Toast.makeText(getApplicationContext(), getString(R.string.transfering, number), Toast.LENGTH_LONG).show();
+			super.onPreExecute();
+		}
+		
+		@Override
+		protected Integer doInBackground(Void... params) {
+			JSONObject jTransferObject = createJsonTransferObject("transfer", number);
+			Connection.getInstance().sendJsonString(jTransferObject);
+			return null;
+		}
+	}
 }
