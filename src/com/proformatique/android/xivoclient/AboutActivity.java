47cd857d092e03fd056f897644e1012cd126f956
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-02-21 16:31:28 -0500

    AboutActivity fix coding style
    Files from version 1.0 were not compliant to the Android or java coding style
    switching to the android coding style.

--- src/com/proformatique/android/xivoclient/AboutActivity.java
+++ src/com/proformatique/android/xivoclient/AboutActivity.java
@@ -24,61 +24,61 @@ import android.util.Log;
 import android.widget.TextView;
 
 public class AboutActivity extends XivoActivity {
-	
-	private final static String TAG = "XiVO About";
-	
-	private final static double KB = Math.pow(2, 10);
-	private final static double MB = Math.pow(2, 20);
-	private final static double GB = Math.pow(2, 30);
-	private final static double TB = Math.pow(2, 40);
-	
-	TextView bandwidth;
-	TextView unit;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-		super.registerButtons();
-		
-		bandwidth = (TextView) findViewById(R.id.bandwidth_received);
-		unit = (TextView) findViewById(R.id.bandwidth_unit);
-	}
-	
-	@Override
-	protected void onBindingComplete() {
-		super.onBindingComplete();
-		
-		long received = -1L;
-		
-		try {
-			if (xivoConnectionService != null)
-				received = xivoConnectionService.getReceivedBytes();
-		} catch (Exception e) {
-			Log.d(TAG, "Could not retrieve bandwidth, using default value");
-		}
-		
-		if (received == -1L) {
-			bandwidth.setText(getString(R.string.unknown));
-			unit.setText("");
-		} else if (received == 0) {
-			bandwidth.setText(Long.toString(received));
-			unit.setText(R.string.unit_byte);
-		} else if (received <= KB) {
-			bandwidth.setText(Long.toString(received));
-			unit.setText(R.string.unit_byte + "s");
-		} else if (received <= MB) {
-			bandwidth.setText(String.format("%.2f", received / KB));
-			unit.setText(R.string.unit_kb);
-		} else if (received <= GB) {
-			bandwidth.setText(String.format("%.2f", received / MB));
-			unit.setText(R.string.unit_mb);
-		} else if (received <= TB) {
-			bandwidth.setText(String.format("%.2f", received / GB));
-			unit.setText(R.string.unit_gb);
-		} else {
-			bandwidth.setText(String.format("%.2f", received / TB));
-			unit.setText(R.string.unit_tb);
-		}
-	}
+    
+    private final static String TAG = "XiVO About";
+    
+    private final static double KB = Math.pow(2, 10);
+    private final static double MB = Math.pow(2, 20);
+    private final static double GB = Math.pow(2, 30);
+    private final static double TB = Math.pow(2, 40);
+    
+    private TextView mBandwidth;
+    private TextView mUnit;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        super.registerButtons();
+        
+        mBandwidth = (TextView) findViewById(R.id.bandwidth_received);
+        mUnit = (TextView) findViewById(R.id.bandwidth_unit);
+    }
+    
+    @Override
+    protected void onBindingComplete() {
+        super.onBindingComplete();
+        
+        long received = -1L;
+        
+        try {
+            if (xivoConnectionService != null)
+                received = xivoConnectionService.getReceivedBytes();
+        } catch (Exception e) {
+            Log.d(TAG, "Could not retrieve bandwidth, using default value");
+        }
+        
+        if (received == -1L) {
+            mBandwidth.setText(getString(R.string.unknown));
+            mUnit.setText("");
+        } else if (received == 0) {
+            mBandwidth.setText(Long.toString(received));
+            mUnit.setText(R.string.unit_byte);
+        } else if (received <= KB) {
+            mBandwidth.setText(Long.toString(received));
+            mUnit.setText(R.string.unit_byte + "s");
+        } else if (received <= MB) {
+            mBandwidth.setText(String.format("%.2f", received / KB));
+            mUnit.setText(R.string.unit_kb);
+        } else if (received <= GB) {
+            mBandwidth.setText(String.format("%.2f", received / MB));
+            mUnit.setText(R.string.unit_mb);
+        } else if (received <= TB) {
+            mBandwidth.setText(String.format("%.2f", received / GB));
+            mUnit.setText(R.string.unit_gb);
+        } else {
+            mBandwidth.setText(String.format("%.2f", received / TB));
+            mUnit.setText(R.string.unit_tb);
+        }
+    }
 }
