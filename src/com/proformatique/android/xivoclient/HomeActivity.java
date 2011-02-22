891ac9db1d99a404afa4fc9f9abbd199d73c2f41
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-02-22 08:54:24 -0500

    HomeActivity coding style fix

--- src/com/proformatique/android/xivoclient/HomeActivity.java
+++ src/com/proformatique/android/xivoclient/HomeActivity.java
@@ -19,9 +19,6 @@
 
 package com.proformatique.android.xivoclient;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -39,6 +36,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.proformatique.android.xivoclient.service.CapaxletsProvider;
 import com.proformatique.android.xivoclient.tools.Constants;
 import com.proformatique.android.xivoclient.xlets.XletContactSearch;
@@ -47,203 +47,209 @@ import com.proformatique.android.xivoclient.xlets.XletHisto;
 import com.proformatique.android.xivoclient.xlets.XletServices;
 
 public class HomeActivity extends XivoActivity implements OnItemClickListener {
-	
-	/**
-	 * Constants
-	 */
-	private static final String LOG_TAG = "XiVO Home";
-	
-	/**
-	 * UI
-	 */
-	private GridView grid;
-	private Handler handler = new Handler();
-	
-	/*
-	 * xlets
-	 */
-	private List<String> availXlets = null;
-	private List<String> implementedXlets = null;
-	
-	private XletsAdapter xletsAdapter = null;
-	
-	/**
-	 * Activity life cycle
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.i(LOG_TAG, "onCreate");
-		setContentView(R.layout.home_activity);
-		super.registerButtons();	// Set onClickListeners for the XivoActivity
-		
-		/*
-		 * Setup the grid, it's adapter, observer and listener
-		 */
-		grid = (GridView) findViewById(R.id.grid);
-		xletsAdapter = new XletsAdapter();
-		getContentResolver().registerContentObserver(CapaxletsProvider.CONTENT_URI, true,
-				new XletObserver());
-		grid.setAdapter(xletsAdapter);
-		grid.setOnItemClickListener(this);
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		Log.i(LOG_TAG, "onResume");
-		startInCallScreenKiller(this);
-		xletsAdapter.notifyDataSetChanged();
-	}
-	
-	@Override
-	protected void onDestroy() {
-		Log.d( LOG_TAG, "DESTROY");
-		stopInCallScreenKiller(this);
-		super.onDestroy();
-	}
-	
-	/**
-	 * A runnable to be used from non UI thread to update the Grid
-	 * Use handler.post(upgradeGrid)
-	 */
-	final Runnable updateGrid = new Runnable() {
-		public void run() {
-			if (xletsAdapter != null)
-				xletsAdapter.notifyDataSetChanged();
-		}
-	};
-	
-	/**
-	 * InCallScreenKiller service lifecycle
-	 */
-	public static void startInCallScreenKiller(Context context) {
-		Intent inCallScreenKillerIntent = new Intent();
-		inCallScreenKillerIntent.setClassName(Constants.PACK, InCallScreenKiller.class.getName());
-		context.startService(inCallScreenKillerIntent);
-		Log.d(LOG_TAG, "InCallScreenKiller started");
-	}
-	
-	public static void stopInCallScreenKiller(Context context) {
-		Intent inCallScreenKillerIntent = new Intent();
-		inCallScreenKillerIntent.setClassName(Constants.PACK, InCallScreenKiller.class.getName());
-		context.stopService(inCallScreenKillerIntent);
-		Log.d(LOG_TAG, "InCallScreenKilled stopped");
-	}
-	
-	private class XletsAdapter extends BaseAdapter {
-		
-		public XletsAdapter() {
-			// Add more xlets here
-			implementedXlets = new ArrayList<String>(1);
-			implementedXlets.add("dial");
-			implementedXlets.add("search");
-			implementedXlets.add("history");
-			implementedXlets.add("features");
-			
-			updateAvailableXlets();
-		}
-		
-		/**
-		 * Retrieves the list of xlets from the content provider
-		 */
-		public void updateAvailableXlets() {
-			Uri allXlets = CapaxletsProvider.CONTENT_URI;
-			Cursor c = managedQuery(allXlets, null, null, null, null);
-			availXlets = new ArrayList<String>(c.getCount());
-			if (c.moveToFirst()) {
-				do {
-					String incomingXlet = c.getString(c.getColumnIndex(CapaxletsProvider.XLET));
-					int index;
-					if ((index = incomingXlet.indexOf("-")) != -1) {
-						incomingXlet = incomingXlet.substring(0, index);
-					}
-					// Only add xlets that are implemented
-					if (implementedXlets.contains(incomingXlet))
-						availXlets.add(incomingXlet);
-				} while (c.moveToNext());
-			}
-			c.close();
-			handler.post(updateGrid);
-		}
-		
-		@Override
-		public int getCount() {
-			return availXlets == null ? 0 : availXlets.size();
-		}
-		
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
-		
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
-		
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View v;
-			if (convertView == null) {
-				LayoutInflater li = getLayoutInflater();
-				v = li.inflate(R.layout.icon, null);
-			} else {
-				v = convertView;
-			}
-			TextView tv = (TextView) v.findViewById(R.id.icon_text);
-			ImageView iv = (ImageView) v.findViewById(R.id.icon_image);
-			
-			if (availXlets.get(position).equals("dial")) {
-				tv.setText(getString(R.string.dialer_btn_lbl));
-				iv.setImageResource(R.drawable.ic_menu_call);
-			} else if (availXlets.get(position).equals("search")) {
-				tv.setText(getString(R.string.userslist_btn_lbl));
-				iv.setImageResource(R.drawable.ic_menu_friendslist);
-			} else if (availXlets.get(position).equals("history")) {
-				tv.setText(getString(R.string.history_btn_lbl));
-				iv.setImageResource(R.drawable.ic_menu_recent_history);
-			} else if (availXlets.get(position).equals("features")) {
-				tv.setText(getString(R.string.service_btn_lbl));
-				iv.setImageResource(R.drawable.ic_menu_manage);
-			} else {
-				tv.setText("...");
-				iv.setImageResource(R.drawable.icon);
-			}
-			return v;
-		}
-	}
-	
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		String choice = availXlets.get(position);
-		Log.d(LOG_TAG, "Clicked " + choice);
-		if (choice.equals("dial")) {
-			Intent i = new Intent(this, XletDialer.class);
-			startActivity(i);
-		} else if (choice.equals("features")) {
-			Intent i = new Intent(this, XletServices.class);
-			startActivity(i);
-		} else if (choice.equals("search")) {
-			Intent i = new Intent(this, XletContactSearch.class);
-			startActivity(i);
-		} else if (choice.equals("history")) {
-			startActivity(new Intent(this, XletHisto.class));
-		} else {
-			Log.d(LOG_TAG, "Unhandled click");
-		}
-	}
-	
-	private class XletObserver extends android.database.ContentObserver {
-		
-		public XletObserver() {
-			super(null);
-		}
-		
-		@Override
-		public void onChange(boolean selfChange) {
-			super.onChange(selfChange);
-			xletsAdapter.updateAvailableXlets();
-		}
-	}
+    
+    /**
+     * Constants
+     */
+    private static final String LOG_TAG = "XiVO Home";
+    
+    /**
+     * UI
+     */
+    private GridView grid;
+    private Handler handler = new Handler();
+    private XivoNotification xivoNotif;
+    
+    /*
+     * xlets
+     */
+    private List<String> availXlets = null;
+    private List<String> implementedXlets = null;
+    
+    private XletsAdapter xletsAdapter = null;
+    
+    /**
+     * Activity life cycle
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(LOG_TAG, "onCreate");
+        setContentView(R.layout.home_activity);
+        super.registerButtons(); // Set onClickListeners for the XivoActivity
+        
+        /*
+         * Setup the grid, it's adapter, observer and listener
+         */
+        grid = (GridView) findViewById(R.id.grid);
+        xletsAdapter = new XletsAdapter();
+        getContentResolver().registerContentObserver(CapaxletsProvider.CONTENT_URI, true,
+                new XletObserver());
+        grid.setAdapter(xletsAdapter);
+        grid.setOnItemClickListener(this);
+        xivoNotif = new XivoNotification(getApplicationContext());
+        xivoNotif.createNotification();
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+        startInCallScreenKiller(this);
+        xletsAdapter.notifyDataSetChanged();
+    }
+    
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "DESTROY");
+        stopInCallScreenKiller(this);
+        super.onDestroy();
+        xivoNotif.removeNotif();
+    }
+    
+    /**
+     * A runnable to be used from non UI thread to update the Grid Use
+     * handler.post(upgradeGrid)
+     */
+    final Runnable updateGrid = new Runnable() {
+        
+        public void run() {
+            if (xletsAdapter != null) {
+                xletsAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+    
+    /**
+     * InCallScreenKiller service lifecycle
+     */
+    public static void startInCallScreenKiller(Context context) {
+        Intent inCallScreenKillerIntent = new Intent();
+        inCallScreenKillerIntent.setClassName(Constants.PACK, InCallScreenKiller.class.getName());
+        context.startService(inCallScreenKillerIntent);
+        Log.d(LOG_TAG, "InCallScreenKiller started");
+    }
+    
+    public static void stopInCallScreenKiller(Context context) {
+        Intent inCallScreenKillerIntent = new Intent();
+        inCallScreenKillerIntent.setClassName(Constants.PACK, InCallScreenKiller.class.getName());
+        context.stopService(inCallScreenKillerIntent);
+        Log.d(LOG_TAG, "InCallScreenKilled stopped");
+    }
+    
+    private class XletsAdapter extends BaseAdapter {
+        
+        public XletsAdapter() {
+            // Add more xlets here
+            implementedXlets = new ArrayList<String>(1);
+            implementedXlets.add("dial");
+            implementedXlets.add("search");
+            implementedXlets.add("history");
+            implementedXlets.add("features");
+            
+            updateAvailableXlets();
+        }
+        
+        /**
+         * Retrieves the list of xlets from the content provider
+         */
+        public void updateAvailableXlets() {
+            Uri allXlets = CapaxletsProvider.CONTENT_URI;
+            Cursor c = managedQuery(allXlets, null, null, null, null);
+            availXlets = new ArrayList<String>(c.getCount());
+            if (c.moveToFirst()) {
+                do {
+                    String incomingXlet = c.getString(c.getColumnIndex(CapaxletsProvider.XLET));
+                    int index;
+                    if ((index = incomingXlet.indexOf("-")) != -1) {
+                        incomingXlet = incomingXlet.substring(0, index);
+                    }
+                    // Only add xlets that are implemented
+                    if (implementedXlets.contains(incomingXlet))
+                        availXlets.add(incomingXlet);
+                } while (c.moveToNext());
+            }
+            c.close();
+            handler.post(updateGrid);
+        }
+        
+        @Override
+        public int getCount() {
+            return availXlets == null ? 0 : availXlets.size();
+        }
+        
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+        
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+        
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+            if (convertView == null) {
+                LayoutInflater li = getLayoutInflater();
+                v = li.inflate(R.layout.icon, null);
+            } else {
+                v = convertView;
+            }
+            TextView tv = (TextView) v.findViewById(R.id.icon_text);
+            ImageView iv = (ImageView) v.findViewById(R.id.icon_image);
+            
+            if (availXlets.get(position).equals("dial")) {
+                tv.setText(getString(R.string.dialer_btn_lbl));
+                iv.setImageResource(R.drawable.ic_menu_call);
+            } else if (availXlets.get(position).equals("search")) {
+                tv.setText(getString(R.string.userslist_btn_lbl));
+                iv.setImageResource(R.drawable.ic_menu_friendslist);
+            } else if (availXlets.get(position).equals("history")) {
+                tv.setText(getString(R.string.history_btn_lbl));
+                iv.setImageResource(R.drawable.ic_menu_recent_history);
+            } else if (availXlets.get(position).equals("features")) {
+                tv.setText(getString(R.string.service_btn_lbl));
+                iv.setImageResource(R.drawable.ic_menu_manage);
+            } else {
+                tv.setText("...");
+                iv.setImageResource(R.drawable.icon);
+            }
+            return v;
+        }
+    }
+    
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String choice = availXlets.get(position);
+        Log.d(LOG_TAG, "Clicked " + choice);
+        if (choice.equals("dial")) {
+            Intent i = new Intent(this, XletDialer.class);
+            startActivity(i);
+        } else if (choice.equals("features")) {
+            Intent i = new Intent(this, XletServices.class);
+            startActivity(i);
+        } else if (choice.equals("search")) {
+            Intent i = new Intent(this, XletContactSearch.class);
+            startActivity(i);
+        } else if (choice.equals("history")) {
+            startActivity(new Intent(this, XletHisto.class));
+        } else {
+            Log.d(LOG_TAG, "Unhandled click");
+        }
+    }
+    
+    private class XletObserver extends android.database.ContentObserver {
+        
+        public XletObserver() {
+            super(null);
+        }
+        
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            xletsAdapter.updateAvailableXlets();
+        }
+    }
 }
